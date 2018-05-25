7bea62bdbd7072a0b202bc47c24a8ac499ee132c
akash2415 <akash2415@gmail.com>
2018-05-25 16:44:42 +0530

    Added view and template and detailed view also

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 from . import views
+from .models import Post
 
 urlpatterns = [
 	url(r'^$', views.post_list, name="post_list"),
+	url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 
 ]
